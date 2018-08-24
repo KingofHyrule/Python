9ed9211f7cac017e67302d8492d229dae1fa2430
KingofHyrule <42677799+KingofHyrule@users.noreply.github.com>
2018-08-24 19:08:02 +0300

    There is some problems but i will solve them ><

--- Python Calculator (Private).py
+++ Python Calculator (Private).py
@@ -1,36 +1,55 @@
 from tkinter import *
 import math
+import time
 
 hesapmakinesi = Tk()
 hesapmakinesi.title("Hesap Makinesi")
 hesapmakinesi.configure(background="gray")
 
 sayi_gir_1 = Entry(hesapmakinesi, width=40, bg="white")
-sayi_gir_1.grid(row=2, column=0, sticky=W)
+sayi_gir_1.grid(row=2, column=5, sticky=W)
+Label(hesapmakinesi, text="1. Sayı:", bg="gray", fg="white", font="none 12 bold") .grid(row=2, column=0, sticky=W)
 
 sayi_gir_2 = Entry(hesapmakinesi, width=40, bg="white")
-sayi_gir_2.grid(row=4, column=0, sticky=W)
+sayi_gir_2.grid(row=3, column=5, sticky=W)
+Label(hesapmakinesi, text="2. Sayı:", bg="gray", fg="white", font="none 12 bold") .grid(row=3, column=0, sticky=W)
 
 def kapat():
     hesapmakinesi.destroy()
     exit()
 
-def tamam_1():
-    girilen_sayi_1 = sayi_gir_1.get()
-
-def tamam_2():
-    girilen_sayi_2 = sayi_gir_2.get()
-
 def topla():
-    sonuc = girilen_sayi_1 + girilen_sayi_2
-    Label(hesapmakinesi, text=sonuc, bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=0, sticky=W)
+    girilen_sayi_1 = sayi_gir_1.get()
+    girilen_sayi_2 = sayi_gir_2.get()
+    sonuc = float(girilen_sayi_1) + float(girilen_sayi_2)
+    Label(hesapmakinesi, text="Sonuç:", bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=0, sticky=W)
+    Label(hesapmakinesi, text=sonuc, bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=5, sticky=W)
 
-#Button(hesapmakinesi, text="÷", width=6, command=(a/b)) .grid(row=12, column=6, sticky=W)
-Button(hesapmakinesi, text="+", width=6, command=topla) .grid(row=3, column=6, sticky=W)
-#Button(hesapmakinesi, text="x", width=6, command=(a*b)) .grid(row=9, column=6, sticky=W)
-#Button(hesapmakinesi, text="-", width=6, command=(a-b)) .grid(row=6, column=6, sticky=W)
-Button(hesapmakinesi, text="Çıkış", width=6, command=kapat) .grid(row=2, column=6, sticky=W)
-Button(hesapmakinesi, text="Tamam (1. Sayı İçin (Üstteki))", width=6, command=tamam_1) .grid(row=14, column=6, sticky=W)
-Button(hesapmakinesi, text="Tamam (2. Sayı İçin (Alttaki))", width=6, command=tamam_2) .grid(row=16, column=6, sticky=W)
+def cıkart():
+    girilen_sayi_1 = sayi_gir_1.get()
+    girilen_sayi_2 = sayi_gir_2.get()
+    sonuc = float(girilen_sayi_1) - float(girilen_sayi_2)
+    Label(hesapmakinesi, text="Sonuç:", bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=0, sticky=W)
+    Label(hesapmakinesi, text=sonuc, bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=5, sticky=W)
+
+def carp():
+    girilen_sayi_1 = sayi_gir_1.get()
+    girilen_sayi_2 = sayi_gir_2.get()
+    sonuc = float(girilen_sayi_1) * float(girilen_sayi_2)
+    Label(hesapmakinesi, text="Sonuç:", bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=0, sticky=W)
+    Label(hesapmakinesi, text=sonuc, bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=5, sticky=W)
+
+def bol():
+    girilen_sayi_1 = sayi_gir_1.get()
+    girilen_sayi_2 = sayi_gir_2.get()
+    sonuc = float(girilen_sayi_1) / float(girilen_sayi_2)
+    Label(hesapmakinesi, text="Sonuç:", bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=0, sticky=W)
+    Label(hesapmakinesi, text=sonuc, bg="gray", fg="white", font="none 12 bold") .grid(row=6, column=5, sticky=W)
+
+Button(hesapmakinesi, text="÷", width=15, command=bol) .grid(row=12, column=11, sticky=W)
+Button(hesapmakinesi, text="+", width=15, command=topla) .grid(row=3, column=11, sticky=W)
+Button(hesapmakinesi, text="x", width=15, command=carp) .grid(row=9, column=11, sticky=W)
+Button(hesapmakinesi, text="-", width=15, command=cıkart) .grid(row=6, column=11, sticky=W)
+Button(hesapmakinesi, text="Çıkış", width=15, command=kapat) .grid(row=2, column=11, sticky=W)
 
 hesapmakinesi.mainloop()
